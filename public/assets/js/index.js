a9daa0f9d7b7be379f9dfd4a804395c2b1c4099c
PhucNguyen08 <npnguyen812@gmail.com>
2023-04-27 21:56:10 +0700

    update

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,18 +1,4 @@
-console.log('Hello');
-
-fetch('http://localhost:3000/weather?address=Boston')
-    .then(response => response.json())
-    .then(data => {
-        if (data.error) {
-            console.log(data.error);
-        } else {
-            console.log(data.forecast);
-            console.log(data.location);
-        }
-    });
-
 const weatherForm = document.querySelector('form');
-
 const input = document.querySelector('input');
 
 weatherForm.addEventListener('submit', e => {
